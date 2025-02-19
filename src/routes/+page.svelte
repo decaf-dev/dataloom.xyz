4703695788cfdf23d053f2030f0c210929c72411
DecafDev <40307803+decaf-dev@users.noreply.github.com>
2025-02-19 16:22:05 -0700

    fix: update styles for mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,12 @@
 	<title>DataLoom</title>
 </svelte:head>
 
-<div class="flex h-[calc(100vh-112px)] w-full flex-col items-center justify-center gap-y-10">
-	<h1 class="text-5xl leading-6 font-semibold">DataLoom is back</h1>
-	<p class="text-2xl leading-4">We just need to update the plugin...</p>
-	<p class="mt-2 text-lg leading-4">Please be patient as we work on the next release :)</p>
+<div class="flex h-[calc(100vh-112px)] w-full flex-col items-center justify-center p-4">
+	<h1 class="mb-3 text-3xl leading-9 font-semibold md:mb-10 md:text-5xl">DataLoom is back</h1>
+	<p class="mb-2 text-lg leading-6 md:mb-8 md:text-2xl md:leading-4">
+		We just need to update the plugin...
+	</p>
+	<p class="text-sm leading-6 md:text-lg md:leading-4">
+		Please be patient as we work on the next release :)
+	</p>
 </div>
