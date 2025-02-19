d77b001b3be5d44d82e8d986836938aade038643
DecafDev <40307803+decaf-dev@users.noreply.github.com>
2025-02-19 02:28:10 -0700

    feat: add in progress message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,9 +8,9 @@
 
 <div class="flex h-[calc(100vh-112px)] w-full flex-col items-center justify-center gap-y-8">
 	<h1 class="text-3xl leading-6 font-semibold">DataLoom</h1>
-	<p class="text-lg leading-4">Obsidian tables don't cut it</p>
+	<p class="text-lg leading-4">Obsidian tables just don't cut it. Let's try something better</p>
 	<button
-		class="cursor-pointer rounded-lg border border-gray-500 px-3 py-2"
+		class="cursor-pointer rounded-lg border border-gray-500 px-4 py-3"
 		onclick={() => goto('/docs/quickstart')}>Get started</button
 	>
 </div>
