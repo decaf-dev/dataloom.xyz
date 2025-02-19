8744d046f09fe3417f17cd597e3d88310ff03e78
DecafDev <40307803+decaf-dev@users.noreply.github.com>
2025-02-19 03:37:27 -0700

    feat: update the website homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,12 @@
 <script>
-	import { goto } from '$app/navigation';
 </script>
 
 <svelte:head>
 	<title>DataLoom</title>
 </svelte:head>
 
-<div class="flex h-[calc(100vh-112px)] w-full flex-col items-center justify-center gap-y-8">
-	<h1 class="text-3xl leading-6 font-semibold">DataLoom</h1>
-	<p class="text-lg leading-4">
-		Native Obsidian tables just don't cut it. Introducing something better...
-	</p>
-	<button
-		class="cursor-pointer rounded-lg border border-gray-500 px-4 py-3"
-		onclick={() => goto('/docs/quickstart')}>Get started</button
-	>
+<div class="flex h-[calc(100vh-112px)] w-full flex-col items-center justify-center gap-y-10">
+	<h1 class="text-5xl leading-6 font-semibold">DataLoom is back</h1>
+	<p class="text-2xl leading-4">We just need to update the plugin...</p>
+	<p class="mt-2 text-lg leading-4">Please be patient as we work on the next release :)</p>
 </div>
